6c008416d9005647c5d218e211e9ee4fc3531ca2
Zafer Çetin <zaferkurumsal@gmail.com>
2023-10-12 21:43:26 +0300

    first commit

---
+++ android/app/src/main/kotlin/dev/igzafer/devnote/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.igzafer.devnote
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:devnote/util/constant/color_constant.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: ColorConstant.APP_BLACK,
+      ),
+    );
+  }
+}
